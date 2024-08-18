370f0f68db80d47563d420d2f8d2beee9972e8f3
sukhiboi <41sukhdevsingh@gmail.com>
2024-08-18 05:39:39 +0530

    Add main game logic... WIP still buggy

--- js/othello.js
+++ js/othello.js
@@ -32,9 +32,68 @@ const areAllCoinsUsed = (board) => {
   return true
 }
 
-const placeCoin = ({ board, player: { symbol } }, { rowIdx, cellIdx }) => {
-  board[rowIdx][cellIdx] = symbol;
+const computeCoinsToChange = ({ rowIdx, cellIdx }, nextSameCoint, { x, y }) => {
+  let numberOfCoins = 0
+  if (x > 0 && y > 0) {
+    numberOfCoins = Math.max(rowIdx - 1 + nextSameCoint.rowIdx, cellIdx - 1 + nextSameCoint.cellIdx)
+  } else {
+    numberOfCoins = Math.max(nextSameCoint.rowIdx - 1 - rowIdx, nextSameCoint.cellIdx - 1 - cellIdx)
+  }
+
+  return Array.from({ length: numberOfCoins }).reduce((coins, _) => {
+    const lastCoin = coins[coins.length - 1];
+    return [...coins, { rowIdx: lastCoin.rowIdx + x, cellIdx: lastCoin.cellIdx + y }];
+  }, [{ rowIdx: rowIdx + x, cellIdx: cellIdx + y }])
+}
+
+const highEndCond = ([rowIdx, cellIdx]) => {
+  return rowIdx === 8 || cellIdx === 8
+}
+
+const lowEndCond = ([rowIdx, cellIdx]) => {
+  return rowIdx < 0 || cellIdx < 0
+}
+
+const updateCoins = (board, { rowIdx, cellIdx }, { x, y }, symbol) => {
+  if (highEndCond([rowIdx + x, cellIdx + y])) return board;
+  if (lowEndCond([rowIdx + x, cellIdx + y])) return board;
+
+  let nextSameCoin = undefined
+  let currentSearchPlace = [rowIdx + x, cellIdx + y];
+
+
+  while (!highEndCond(currentSearchPlace) && !lowEndCond(currentSearchPlace)) {
+    if (board[currentSearchPlace[0]][currentSearchPlace[1]] === symbol) {
+      nextSameCoin = { rowIdx: currentSearchPlace[0], cellIdx: currentSearchPlace[1] };
+      break;
+    }
+    if (board[currentSearchPlace[0]][currentSearchPlace[1]] === undefined) break;
+    currentSearchPlace = [currentSearchPlace[0] + x, currentSearchPlace[1] + y]
+  }
+
+  if (nextSameCoin === undefined) return board;
+
+  computeCoinsToChange({ rowIdx, cellIdx }, nextSameCoin, { x, y }).forEach(coin => {
+    board[coin.rowIdx][coin.cellIdx] = symbol;
+  })
+
   return board;
+}
+
+const placeCoin = ({ board, player: { symbol } }, coin) => {
+  board[coin.rowIdx][coin.cellIdx] = symbol;
+  return [
+    { x: 0, y: 1 },
+    { x: 1, y: 0 },
+    { x: 1, y: 1 },
+    { x: 0, y: -1 },
+    { x: -1, y: 0 },
+    { x: -1, y: -1 },
+    { x: -1, y: 1 },
+    { x: 1, y: -1 },
+  ].reduce((acc, movement) => {
+    return updateCoins(acc, coin, movement, symbol)
+  }, board)
 }
 
 const findWinner = ({ board }, players) => {
